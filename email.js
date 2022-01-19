63076e1b73a47e08b49f667c0f2afac2a5d2424e
Deepashri Dali <Deepashri.Dali@softwire.com>
2022-01-19 22:26:17 +0000

    initial commit

---
+++ email.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const file = fs.readFileSync("test.txt", {encoding:"utf8"});
+// console.log(file);
+const regexFindEmail = /@[a-zA-Z0-9.-]+/g;
+
+
+let regexFindEmailFile = file.match(regexFindEmail);
+console.log(regexFindEmailFile.sort());
+```
+const count = {};
+  regexFindEmailFile.forEach(element => {
+    count[element] = (count[element] || 0) + 1;
+  });
+console.log(count);
+
+
+const sortedEmails = 
+    Object.entries(count).sort(([,a],[,b]) => b-a);
+
+console.log(sortedEmails.slice(0,10));
+```
+const regexSoft = /@softwire\.com\b/g;
+const regexHot = /@hotmail\.com\b/g;
+// let regexFile = file.match(regex);
+// console.log(regexFile.length);
+function emailExtractor (regexp){
+   let regFile = file.match(regexp);
+   return regFile.length;
+}
+
+console.log(emailExtractor(regexSoft));
+console.log(emailExtractor(regexHot));
+
+// let counter = 0;
+
+
+ 
+// for(let i = 0; i < file.length; i++) {
+//   if ((file.substring(i, i+ 13) === '@softwire.com')) { 
+    
+//       counter++;
+//      }
+// }
+ 
+// console.log(counter);
